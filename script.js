d44156acd1fc6379759b7337c71b1f83f5c4ce0d
Everett Moore <everettmoore991@gmail.com>
2024-01-01 10:31:16 -0600

    Add event listeners for canvas drawing

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
+let mouseIsDown = false;
 const canvasContainer = document.querySelector("#canvasContainer");
 
 for (let i = 0; i < 256; i++) {
   let canvasUnit = document.createElement("div");
   canvasUnit.classList.add("canvas");
   canvasContainer.appendChild(canvasUnit);
+
+  canvasUnit.addEventListener("mousemove", () => {
+    if (mouseIsDown) {
+      canvasUnit.style.backgroundColor = "black";
+    }
+  });
+  canvasUnit.addEventListener("mousedown", () => {
+    mouseIsDown = true;
+  });
+  canvasUnit.addEventListener("mouseup", () => {
+    mouseIsDown = false;
+  });
 }
