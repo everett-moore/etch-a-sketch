e3ba356e62cbf8aa5c0a666fdef22d45411f9201
Everett Moore <everettmoore991@gmail.com>
2024-01-01 10:39:51 -0600

    Add constants for grid and color
    Initial values on launch

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
+const COLOR_DEFAULT = "black";
+const GRID_DEFAULT = 16;
+
+let penColor = COLOR_DEFAULT;
+let gridDimensions = GRID_DEFAULT;
+
 let mouseIsDown = false;
+
 const canvasContainer = document.querySelector("#canvasContainer");
 
-for (let i = 0; i < 256; i++) {
+for (let i = 0; i < gridDimensions * gridDimensions; i++) {
   let canvasUnit = document.createElement("div");
   canvasUnit.classList.add("canvas");
   canvasContainer.appendChild(canvasUnit);
 
   canvasUnit.addEventListener("mousemove", () => {
     if (mouseIsDown) {
-      canvasUnit.style.backgroundColor = "black";
+      canvasUnit.style.backgroundColor = penColor;
     }
   });
   canvasUnit.addEventListener("mousedown", () => {
