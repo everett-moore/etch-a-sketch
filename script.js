c14e04566bd146be8177711a544d7e97b2d96aff
Everett Moore <everettmoore991@gmail.com>
2024-01-02 18:45:32 -0600

    Wrap code in dividing functions

--- script.js
+++ script.js
@@ -3,35 +3,40 @@ const GRID_DEFAULT = 16;
 
 let penColor = COLOR_DEFAULT;
 let gridDimensions = GRID_DEFAULT;
-
 let mouseIsDown = false;
 
 const canvasContainer = document.querySelector("#canvasContainer");
 const colorPicker = document.querySelector("#colorPicker");
 
-for (let i = 0; i < gridDimensions * gridDimensions; i++) {
-  let canvasUnit = document.createElement("div");
-  canvasUnit.classList.add("canvas");
-  canvasUnit.setAttribute("draggable", "false");
-  canvasContainer.appendChild(canvasUnit);
-  canvasContainer.addEventListener("mouseleave", () => {
-    mouseIsDown = false;
-  });
-  canvasUnit.addEventListener("mousemove", () => {
-    if (mouseIsDown) {
-      penColor = colorPicker.value;
-      canvasUnit.style.backgroundColor = penColor;
-    }
-  });
-  canvasUnit.addEventListener("mousedown", () => {
-    penColor = colorPicker.value;
-    canvasUnit.style.backgroundColor = penColor;
-  });
-  canvasContainer.addEventListener("mousedown", (e) => {
-    e.preventDefault();
-    mouseIsDown = true;
-  });
-  canvasContainer.addEventListener("mouseup", () => {
-    mouseIsDown = false;
-  });
+function createCanvas() {
+  for (let i = 0; i < gridDimensions * gridDimensions; i++) {
+    let canvasUnit = document.createElement("div");
+    canvasUnit.classList.add("canvas");
+    canvasContainer.appendChild(canvasUnit);
+    canvasContainer.addEventListener("mouseleave", () => {
+      mouseIsDown = false;
+    });
+    canvasUnit.addEventListener("mousemove", () => {
+      if (mouseIsDown) {
+        getBrushColor(canvasUnit);
+      }
+    });
+    canvasUnit.addEventListener("mousedown", () => {
+      getBrushColor(canvasUnit);
+    });
+    canvasContainer.addEventListener("mousedown", (e) => {
+      e.preventDefault();
+      mouseIsDown = true;
+    });
+    canvasContainer.addEventListener("mouseup", () => {
+      mouseIsDown = false;
+    });
+  }
 }
+
+function getBrushColor(canvasUnit) {
+  penColor = colorPicker.value;
+  canvasUnit.style.backgroundColor = penColor;
+}
+
+createCanvas();
